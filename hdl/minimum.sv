9c95f7a138877aeff8e2da1ed021086e5a587aef
Toya Takahashi <toyatakahashi522@gmail.com>
2024-12-02 01:53:59 -0500

    potentially better minimum

--- hdl/minimum.sv
+++ hdl/minimum.sv
@@ -7,21 +7,42 @@ module minimum (
     output logic [2:0] minimum_index
     );
 
-    logic [8:0] min [6:0];
-    logic [2:0] index [6:0];
-    assign minimum_index = index[max-1];
+    logic [8:0] min_0 [7:0];
+    logic [2:0] index_0 [7:0];
+    logic [8:0] min_1 [3:0];
+    logic [2:0] index_1 [3:0];
+    logic [8:0] min_2 [1:0];
+    logic [2:0] index_2 [1:0];
 
     always_comb begin
-        min[0] = vals_in[0];
-        index[0] = 0;
-        for (int i=1; i<7; i=i+1) begin
-            if (vals_in[i] < min[i-1]) begin
-                min[i] = vals_in[i];
-                index[i] = i;
+        for (int i=0; i<7; i=i+1) begin
+            min_0[i] = i < max ? vals_in[i] : 9'b1_1111_1111;
+            index_0[i] = i;
+        end
+        min_0[7] = 9'b1_1111_1111;
+        index_0[7] = 0;
+        for (int j=0; j<4; j=j+1) begin
+            if (min_0[2*j] < min_0[2*j+1]) begin
+                min_1[j] = min_0[2*j];
+                index_1[j] = index_0[2*j];
             end else begin
-                min[i] = min[i-1];
-                index[i] = index[i-1];
+                min_1[j] = min_0[2*j+1];
+                index_1[j] = index_0[2*j+1];
             end
+        end
+        for (int j=0; j<2; j=j+1) begin
+            if (min_1[2*j] < min_1[2*j+1]) begin
+                min_2[j] = min_1[2*j];
+                index_2[j] = index_1[2*j];
+            end else begin
+                min_2[j] = min_1[2*j+1];
+                index_2[j] = index_1[2*j+1];
+            end
+        end
+        if (min_2[0] < min_2[1]) begin
+            minimum_index = index_2[0];
+        end else begin
+            minimum_index = index_2[1];
         end
     end
 endmodule
